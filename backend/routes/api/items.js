3d7b0d10bef62ad7eabfbb73e531054392985b95
anukkchalai <112948265+anukkchalai@users.noreply.github.com>
2022-09-07 09:54:18 +0530

    Update items.js

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -6,7 +6,7 @@ var User = mongoose.model("User");
 var auth = require("../auth");
 const { sendEvent } = require("../../lib/event");
 
-// Preload item objects on routes with ':item'
+//  Preload item objects on routes with ':item'
 router.param("item", function (req, res, next, slug) {
     Item.findOne({ slug: slug })
         .populate("seller")
